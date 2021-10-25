99466f7f0e132ea991af1ff52e88b5078534dfee
Mohamed Kuthubudeen <kuthub1989@gmail.com>
2021-10-25 13:33:18 +0530

    Request Response - Grouping Routes

--- routes/web.php
+++ routes/web.php
@@ -53,6 +53,7 @@ Route::get('/post/{id}', function ($id) use ($posts) {
 
 //PostList
 Route::get('/post', function () use ($posts) {
+    dd(request()->all());
     return view('post.index', ['posts' => $posts]);
 });
 
@@ -62,3 +63,43 @@ Route::get('/recent-posts/{daysAgo?}', function ($daysAgo = 5) {
 })
     ->where(['daysAgo' => '[0-9]+'])
     ->name("Dynamic Route with Optional Parameter");
+
+//Grouping Routes of Same Prefix URL
+Route::prefix('/test')->name('test.')->group(function () use ($posts) {
+    //Request and Reponse Test
+    Route::get('/response', function () use ($posts) {
+        return response($posts, 201)
+            ->header('Content-Type', 'application/json')
+            ->cookie('MY_COOKIE', 'Kuthub', 3600);
+    })->name('reponses');
+
+    //Redirect
+    Route::get('/redirect', function () {
+        return redirect('/contact');
+    })->name('redirect');
+
+    //Back
+    Route::get('/back', function () {
+        return back();
+    })->name('back');
+
+    //Redirect Named Route
+    Route::get('/named-redirect', function () {
+        return redirect()->route('SinglePost', ['id' => 2]);
+    })->name('Named-Redirect');
+
+    //Redirect Away
+    Route::get('/away', function () {
+        return redirect()->away('https://google.com');
+    })->name('away');
+
+    //JSON Reponse
+    Route::get('/json', function () use ($posts) {
+        return response()->json($posts);
+    })->name('json');
+
+    //Download Reponse
+    Route::get('/download', function () {
+        return response()->download(public_path('/images/profile.jpg', 'face.jpg'));
+    })->name('download');
+});
